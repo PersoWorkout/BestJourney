9640fdcae8cb3d3e4f9349bf0a0f70e414b954b1
Yasin2719 <karakusyasin19@gmail.com>
2024-01-26 16:29:20 +0100

    feat(domain.dtos): add and test UpdateUserValidator class

---
+++ Test/Unit/Domain.UnitTest/DTOs/UpdateUserValidatorTest.cs
@@ -0,0 +1,49 @@
+﻿using Domain.DTOs.Validators;
+
+namespace Domain.UnitTest.DTOs
+{
+    public class UpdateUserValidatorTest
+    {
+        [Fact]
+        public void Validate_ShouldBeTrue_WhenPayloadIsValid()
+        {
+            //Arrange
+            var payload = CreateValidPayload();
+
+            //Act
+            var result = payload.Validate();
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Validate_ShouldBeFalse_WhenPayloadIsEmpty()
+        {
+            //Arrange
+            var payload = CreateInvalidPayload();
+
+            //Act
+            var result = payload.Validate();
+
+            //Assert
+            Assert.False(result);
+        }
+
+        private static UpdateUserValidator CreateValidPayload()
+        {
+            return new UpdateUserValidator
+            {
+                Firstname = "John",
+                Lastname = "Doe",
+                Email = "john.doe@example.com",
+                Password = "Password123!"
+            };
+        }
+
+        private static UpdateUserValidator CreateInvalidPayload()
+        {
+            return new UpdateUserValidator();
+        }
+    }
+}
---
+++ src/Domain/DTOs/Validators/UpdateUserValidator.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Utils;
+
+namespace Domain.DTOs.Validators
+{
+    public class UpdateUserValidator
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public bool Validate()
+        {
+            var test = Email.IsValidEmail();
+            return Firstname.IsValid() ||
+                Lastname.IsValid() ||
+                Email.IsValidEmail() ||
+                Password.IsValidPassword();
+        }
+    }
+}
