f60f1a5b69a50f2afcbc0af7bca8b578aeff88a9
Yasin2719 <karakusyasin19@gmail.com>
2024-01-27 00:00:00 +0100

    feat(domain.errors): add user errors

---
+++ src/Domain/Errors/Users/UserError.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Errors.Users
+{
+    public static class UserError
+    {
+        public static readonly Error InvalidPayload = new("Users.InvalidPayload", "The payload is invalid");
+        public static readonly Error EmailAlreadyUsed = new("Users.EmailAlreadyUsed", "The email is already used");
+        public static readonly Error NotFound = new("Users.NotFound", "The user was not found");
+        public static readonly Error InvalidCredentials = new("Users.InvalidCredentials", "The email or password is invalid");
+    }
+}
