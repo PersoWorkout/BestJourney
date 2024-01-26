5e4071a26f4f7e2f2edacf0d599092dabb059536
Yasin2719 <karakusyasin19@gmail.com>
2024-01-26 15:44:26 +0100

    feat(domain.dto): add UserResponse class

---
+++ src/Domain/DTOs/Responses/UserResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.DTOs.Responses
+{
+    public class UserResponse
+    {
+        public string Id { get; set; }
+        public string Firstame { get; set; }
+        public string Lastame { get; set; }
+        public string Email { get; set; }
+    }
+}
