376db380ce55591845913388d6aba3142e400e66
Yasin2719 <karakusyasin19@gmail.com>
2024-01-26 11:32:37 +0100

    fix(domain.user): create and test update method

---
+++ Test/Unit/Domain.UnitTest/Models/UserTest.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Models;
+
+namespace Domain.UnitTest.Models
+{
+    public class UserTest
+    {
+        private const string FIRSTNAME = "John";
+        private const string LASTNAME = "Doe";
+        private const string EMAIL = "john.doe@example.com";
+        private const string PASSWORD = "password";
+
+        [Fact]
+        public void CreateUser_ShouldCreateUser()
+        {
+            //Arrange
+            //Act
+            var user = InstanceNewUser();
+
+            //Assert
+            Assert.True(user.Firstname == FIRSTNAME);
+            Assert.True(user.Lastname == LASTNAME);
+            Assert.True(user.Email == EMAIL);
+            Assert.True(user.Password == PASSWORD);
+            Assert.True(user.CreatedAt < DateTime.Now);
+        }
+
+        [Fact]
+        public void UpdateUser_ShouldUpdateUser()
+        {
+            //Arrange          
+            var user = InstanceNewUser();
+
+            string newFirstname = "Yasin";
+            string newLastname = "Karakus";
+            string newEmail = "yasin.karakus@example.com";
+
+            //Act
+            user.Update(newFirstname, newLastname, newEmail);
+
+            //Assert
+            Assert.True(user.Firstname == newFirstname);
+            Assert.True(user.Lastname == newLastname);
+            Assert.True(user.Email == newEmail);
+            Assert.NotNull(user.UpdatedAt);
+        }
+
+        public User InstanceNewUser()
+        {
+            string firstname = FIRSTNAME;
+            string lastname = LASTNAME;
+            string email = EMAIL;
+            string password = PASSWORD;
+
+            return new(firstname, lastname, email, password);
+        }
+    }
+}
--- src/Domain/Models/User.cs
+++ src/Domain/Models/User.cs
@@ -2,15 +2,27 @@
 
 namespace Domain.Models
 {
-    public class User: IUser
+    public class User( 
+        string firstname,
+        string lastname, 
+        string email, 
+        string password) : IUser
     {
-        public Guid Id { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime? UpdatedAt {  get; set; }
-        public List<UserJourney> UsersJourneys { get; set; }
+        public Guid Id { get; set; } = new Guid();
+        public string Firstname { get; set; } = firstname;
+        public string Lastname { get; set; } = lastname;
+        public string Email { get; set; } = email;
+        public string Password { get; set; } = password;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+        public List<UserJourney> UsersJourneys { get; set; } = [];
+
+        public void Update(string firstname, string lastname, string email)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Email = email;
+            UpdatedAt = DateTime.Now;
+        }
     }
 }
