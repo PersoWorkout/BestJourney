14b70da299ea81a4d7c295f7387ae9f6a77e3d6d
Yasin2719 <karakusyasin19@gmail.com>
2024-01-27 10:52:29 +0100

    feat(application): add service installer

---
+++ src/Application/DependencyInjection/ApplicationServiceInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interfaces.Users;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application.DependencyInjection
+{
+    public static class ApplicationServiceInstaller
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
