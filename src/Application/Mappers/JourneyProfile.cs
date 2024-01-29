6e9e3e8468408a8245244423fc5e82bc767e169b
Yasin2719 <karakusyasin19@gmail.com>
2024-01-29 23:28:53 +0100

    feat(application.Mappers): add journeyProfile

---
+++ src/Application/Mappers/JourneyProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Domain.DTOs.Responses;
+using Domain.DTOs.Validators.Journeys;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers
+{
+    public class JourneyProfile: Profile
+    {
+        public JourneyProfile()
+        {
+            CreateMap<CreateJourneyValidator, Journey>();
+            CreateMap<Journey, JourneyResponse>();
+        }
+    }
+}
