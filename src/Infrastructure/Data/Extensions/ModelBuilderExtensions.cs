d3cb621bd7c8d20a091958b94a609b2e70f184e4
Yasin2719 <karakusyasin19@gmail.com>
2024-02-01 09:31:58 +0100

    feat(infrastructure.data): add model builder extensions

--- src/Infrastructure/Data/Extensions/ModelBuilderExtensions.cs
+++ src/Infrastructure/Data/Extensions/ModelBuilderExtensions.cs
@@ -1,9 +1,5 @@
 ﻿using Domain.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 namespace Infrastructure.Data.Extensions
 {
     public static class ModelBuilderExtensions
