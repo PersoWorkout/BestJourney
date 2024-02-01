e0c1e1dc6b962206d716620e432fa91500dfc4dd
Yasin2719 <karakusyasin19@gmail.com>
2024-02-01 09:33:24 +0100

    feat(api): add journey and user controllers

---
+++ src/API/Controllers/JourneyController.cs
@@ -0,0 +1,81 @@
+﻿using Application.Interfaces.Journeys;
+using Domain.DTOs.Validators.Journeys;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("/journeys")]
+    public class JourneyController(IJourneyService journeyService) : 
+        Controller
+    {
+        private readonly IJourneyService _journeyService = journeyService;
+
+        [HttpGet]
+        public async Task<IResult> Index()
+        {
+            var journeys = await _journeyService.GetJourneys();
+            return Results.Ok(journeys.Data);
+        }
+
+        [HttpPost]
+        public async Task<IResult> Create([FromBody] CreateJourneyValidator payload)
+        {
+            var journey = await _journeyService.Create(payload);
+            return journey.IsSucess ?
+                Results.Created() :
+                Results.Problem(
+                    statusCode: StatusCodes.Status400BadRequest,
+                    title: "Bad Request",
+                    extensions: new Dictionary<string, object?>
+                    {
+                        {"errors", new [] {journey.Error } }
+                    });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IResult> GetById(string id)
+        {
+            var journey = await _journeyService.GetById(id);
+            return journey.IsSucess ?
+                Results.Ok(journey.Data) :
+                Results.Problem(
+                    statusCode: StatusCodes.Status400BadRequest,
+                    title: "Bad Request",
+                    extensions: new Dictionary<string, object?>
+                    {
+                        {"errors", new [] {journey.Error } }
+                    });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IResult> Update(string id, [FromBody] UpdateJourneyValidator payload)
+        {
+            var journey = await _journeyService.Update(id, payload);
+            return journey.IsSucess ?
+                Results.Ok(journey.Data) :
+                Results.Problem(
+                    statusCode: StatusCodes.Status400BadRequest,
+                    title: "Bad Request",
+                    extensions: new Dictionary<string, object?>
+                    {
+                        {"errors", new [] {journey.Error } }
+                    });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IResult> Delete(string id)
+        {
+            var result = await _journeyService.Delete(id);
+            return result.IsSucess ?
+                Results.NoContent() :
+                Results.Problem(
+                    statusCode: StatusCodes.Status400BadRequest,
+                    title: "Bad Request",
+                    extensions: new Dictionary<string, object?>
+                    {
+                        {"errors", new [] {result.Error } }
+                    });
+        }
+    }
+}
---
+++ src/API/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using Application.Interfaces.Users;
+using Domain.DTOs.Responses;
+using Domain.DTOs.Validators.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("/users")]
+    public class UserController(
+        IUserService userService) : Controller
+    {
+        private readonly IUserService _userService = userService;
+
+        //[HttpPost]
+        //public async Task<IResult> Login([FromBody] LoginUserValidator payload)
+        //{
+        //    var result = await _userService.Login(payload);
+
+        //    return result.IsSucess ?
+        //        Results.Ok(result.Data) :
+        //        Results.Problem(
+        //            statusCode: StatusCodes.Status400BadRequest,
+        //            title: "Bad Request",
+        //            extensions: new Dictionary<string, object?>
+        //            {
+        //                {"errors", new [] {result.Error } }
+        //            });
+        //}
+
+        [HttpGet]
+        public async Task<IResult> Get()
+        {
+            var result = await _userService.GetUsers();
+            return result.IsSucess ?
+                Results.Ok(result.Data) :
+                Results.Problem(
+                    statusCode: StatusCodes.Status400BadRequest,
+                    title: "Bad Request",
+                    extensions: new Dictionary<string, object?>
+                    {
+                        {"errors", new [] {result.Error } }
+                    });
+        }
+
+        [HttpPost]
+        public async Task<IResult> Create([FromBody] CreateUserValidator payload)
+        {
+            var result = await _userService.Create(payload);
+
+            return result.IsSucess ?
+                Results.Created() :
+                Results.Problem(
+                    statusCode: StatusCodes.Status400BadRequest,
+                    title: "Bad Request",
+                    extensions: new Dictionary<string, object?>
+                    {
+                        {"errors", new [] {result.Error } }
+                    });
+        }
+    }
+}
