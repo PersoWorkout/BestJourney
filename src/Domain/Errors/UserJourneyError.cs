56a83b55280cf79c589684708e3695f23d55e1fb
Yasin2719 <karakusyasin19@gmail.com>
2024-02-04 19:43:12 +0100

    feat(userjourney): add userJourneyError

---
+++ src/Domain/Errors/UserJourneyError.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Abstractions;
+
+namespace Domain.Errors
+{
+    public class UserJourneyError
+    {
+        public static readonly Error InvalidPayload = new("UserJourney.InvalidPayload", "The payload is invalid");
+        public static readonly Error UserNotFound = new("Users.NotFound", "The user was not found");
+        public static readonly Error JourneyNotFound = new("Users.NotFound", "The user was not found");
+        public static readonly Error JourneyPaied = new("UserJourney.IsPaied", "An order already exist for this journey and you can't update them because it's paied");
+        public static readonly Error PaymentError = new("UserJourney.PaymentError", "An error occured during payment");
+    }
+}
