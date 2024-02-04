15dea9398b66c9974ecbdff4fdff2b1185fb267d
Yasin2719 <karakusyasin19@gmail.com>
2024-02-04 18:53:40 +0100

    feat(userJourney): add status and updatedAt property

---
+++ src/Domain/Enums/PaymentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Enums
+{
+    public enum PaymentStatus
+    {
+        Unpaied = 0,
+        Pending = 1,
+        Paied = 2
+    }
+}
--- src/Domain/Models/UserJourney.cs
+++ src/Domain/Models/UserJourney.cs
@@ -1,14 +1,30 @@
-﻿using Domain.Interfaces;
+﻿using Domain.Enums;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore.Update.Internal;
 
 namespace Domain.Models
 {
-    public class UserJourney: IUserJourney
+    public class UserJourney(
+        Guid userId,
+        Guid journeyId,
+        int peopleNumber): IUserJourney
     {
-        public Guid UserId { get; set; }
-        public Guid JourneyId { get; set; }
-        public int PeopleNumber { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public Guid UserId { get; set; } = userId;
+        public Guid JourneyId { get; set; } = journeyId;
+        public int PeopleNumber { get; set; } = peopleNumber;
+        public PaymentStatus Status { get; set; } = PaymentStatus.Pending;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
         public User User { get; set; }
         public Journey Journey { get; set; }
+
+        public void Update(
+            int peopleNumber,
+            PaymentStatus status)
+        {
+            PeopleNumber = peopleNumber;
+            Status = status;
+            UpdatedAt = DateTime.Now;
+        }
     }
 }
