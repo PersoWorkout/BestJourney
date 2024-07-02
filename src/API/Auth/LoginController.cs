ed37e060b8634f2e3e0a63fb9b788f600ca8e10b
Yasin Karakus <yasin.karakus@sea-invest.org>
2024-07-02 15:42:11 +0200

    refacto(api.auth): split controllers by actions

---
+++ src/API/Auth/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using API.Extensions;
+using Application.Auth;
+using Domain.Auth.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Auth;
+
+[ApiController]
+[Route("auth/login")]
+public class LoginController(IAuthService service) : Controller
+{
+    private readonly IAuthService _authService = service;
+
+    [HttpPost]
+    public async Task<IResult> Handle(LoginUserRequest payload)
+    {
+        var result = await _authService.Login(payload);
+
+        return result.IsSucess ?
+            Results.Ok(result.Data) :
+            ResultExtensions.FailureResult(result);
+    }
+}
---
+++ src/API/Auth/LogoutController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Auth;
+
+[ApiController]
+[Route("/auth")]
+public class LogoutController(IAuthService service) : Controller
+{
+    private readonly IAuthService _service = service;
+
+    [HttpDelete]
+    public async Task<IResult> Handle()
+    {
+        var token = HttpContext.Request
+            .Headers.Authorization.ToString();
+
+        await _service.Logout(token);
+
+        return Results.NoContent();
+    }
+}
