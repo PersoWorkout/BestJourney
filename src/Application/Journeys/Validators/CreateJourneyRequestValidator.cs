55a5bda817a07fefeac25bf4538fc0473f7109eb
Yasin Karakus <yasin.karakus@sea-invest.org>
2024-07-04 16:44:55 +0200

    feat(application.journey): add validator

---
+++ src/Application/Journeys/Validators/CreateJourneyRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Journeys.Requests;
+using FluentValidation;
+
+namespace Application.Journeys.Validators;
+
+public class CreateJourneyRequestValidator: AbstractValidator<CreateJourneyRequest>
+{
+    public CreateJourneyRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty();
+
+        RuleFor(x => x.Description)
+            .NotEmpty();
+
+        RuleFor(x => x.City)
+            .NotEmpty();
+
+        RuleFor(x => x.Country)
+            .NotEmpty();
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0);
+    }
+}
